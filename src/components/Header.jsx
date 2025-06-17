8cd3fc4243a5eac323afde4acaee037f6222f142
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-17 00:26:05 +0000

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl bg-blue-500 h-18 flex items-center pl-4 mb-1.5 font-mono font-bold hover:font-extrabold" >CV Application</h1>
+        </div>
+    )
+}
+
+export default Header
