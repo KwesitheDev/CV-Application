6bce6d5bf819a02b7124021642c521008ed42f71
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-19 00:44:19 +0000

    changed background color and added shadow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div>
-            <h1 className="text-3xl bg-blue-500 h-18 flex items-center pl-4 mb-1.5 font-mono font-bold hover:font-extrabold" >CV Application</h1>
+            <h1 className="text-3xl italic h-18 flex items-center pl-4 mb-1.5 font-mono shadow font-bold focus:outline-none text-gray-800" >CV Application</h1>
         </div>
     )
 }
